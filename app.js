56a2974e3665e709bb9c5bf2a7da33a4535ac3d9
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-01-11 18:52:00 +0100

    🎮 - Initialize game

---
+++ app.js
@@ -0,0 +1,35 @@
+function startGame() {
+  // Initialize variables
+  const gameFrame = document.querySelector('.grille');
+  const gameEnemiesContainer = document.querySelector('.enemy-container');
+  const gamePlayerContainer = document.querySelector('.player-container');
+
+  console.log("Initialize the game");
+
+  // Initialize the game
+  // Show 3 lines of 12 enemys
+  for (let i = 0; i < 3; i++) {
+    // Add the enemy to alien-container div
+    const enemyColumn = document.createElement('div');
+    enemyColumn.classList.add('enemy-column');
+    enemyColumn.classList.add('enemy-column-' + i);
+    gameEnemiesContainer.appendChild(enemyColumn);
+
+    for (let j = 0; j < 12; j++) {
+      // Add the enemy to alien-container div
+      const enemy = document.createElement('div');
+      enemy.classList.add('alien');
+      enemy.classList.add('alien-' + j + '-' + i);
+      enemyColumn.appendChild(enemy);
+    }
+  }
+
+  // Add the player to the game-frame div
+  const player = document.createElement('div');
+  player.classList.add('tireur');
+  gamePlayerContainer.appendChild(player);
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  startGame();
+});
