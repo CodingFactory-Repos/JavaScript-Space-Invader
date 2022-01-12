68bdbfe88ef0c310fe6bf89796c60cf77ddf88d2
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-01-12 10:11:09 +0100

    🛸 - Move player

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-function startGame() {
+function initializeGame() {
     // Initialize variables
     const gameFrame = document.querySelector('.grille');
     const gameEnemiesContainer = document.querySelector('.enemy-container');
     const gamePlayerContainer = document.querySelector('.player-container');
 
-    console.log("Initialize the game");
     console.log(gameFrame);
 
     // Initialize the game
@@ -29,9 +28,51 @@ function startGame() {
     // Add the player to the game-frame div
     const player = document.createElement('div');
     player.classList.add('tireur');
+    player.style.left = '50%';
     gamePlayerContainer.appendChild(player);
 }
 
-document.addEventListener(`DOMContentLoaded`, () => {
-    startGame();
+async function startScript(responseTime) {
+    await initializeGame();
+    console.log("Game initialized (" + (new Date().getTime() - responseTime) + "ms)");
+}
+
+function movePlayer(direction, responseTime) {
+    const player = document.querySelector('.tireur');
+    const playerPosition = player.getBoundingClientRect();
+    const playerLeft = parseInt(player.style.left.replace('%', ''));
+    const speed = 5;
+
+    if (direction === 'left') {
+        if((playerLeft - speed) > (0 - speed)){
+            player.style.left = `${playerLeft - speed}%`;
+            console.log("Player moved left (" + (new Date().getTime() - responseTime) + "ms)");
+        }
+    } else if (direction === 'right') {
+        if((playerLeft + speed) < 100){
+            player.style.left = `${playerLeft + speed}%`;
+            console.log("Player moved right (" + (new Date().getTime() - responseTime) + "ms)");
+        }
+    }
+
+}
+
+function checkKey(e) {
+    // Move the player if the key is pressed
+    switch (e.keyCode) {
+        case 37:
+            movePlayer('left', new Date().getTime());
+            break;
+        case 38:
+            movePlayer('up', new Date().getTime());
+            break;
+        case 39:
+            movePlayer('right', new Date().getTime());
+            break;
+    }
+}
+
+document.addEventListener(`DOMContentLoaded`, (async) => {
+    startScript(new Date().getTime());
+    document.onkeydown = checkKey;
 });
